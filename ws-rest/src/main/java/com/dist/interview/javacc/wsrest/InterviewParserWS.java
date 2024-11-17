2df437bf3b08bb868cbc12bf40035a1216ffcafe
tayssir <tayssir.ghanmi@eniso.u-sousse.tn>
2024-11-17 03:35:01 +0100

    [UPDATED] Presentation layer added CRUD operations for the candidate

---
+++ ws-rest/src/main/java/com/dist/interview/javacc/wsrest/InterviewParserWS.java
@@ -0,0 +1,31 @@
+package com.dist.interview.javacc.wsrest;
+
+import com.dist.interview.javacc.infra.model.Candidate;
+import com.dist.interview.javacc.serviceapi.InterviewParserModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parser")
+public class InterviewParserWS {
+    @Autowired
+    private InterviewParserModule parserModule;
+    @PostMapping("/add-candidate")
+    public String addCandidate(@RequestBody Candidate candidate) {
+        return parserModule.addCandidate(candidate);
+    }
+    @GetMapping("/find-candidates")
+    public List<Candidate> findCandidate() {
+        return parserModule.findAllCandidates();
+    }
+    @GetMapping("/test")
+    public String hello() {
+        return "Congratulations you did it \uD83C\uDF89\uD83C\uDF89";
+    }
+    @DeleteMapping("/delete/{id}")
+    public String  removeCandidate(@RequestBody Candidate candidate) {
+        return parserModule.deleteCandidate(candidate.getId());
+    }
+}
